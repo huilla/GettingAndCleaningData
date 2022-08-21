832adbd5998756d2f4d849e2d19b034c1798c00b
huilla <julia.huilla@gmail.com>
2022-08-22 00:54:27 +0300

    added steps 2 and 3

--- run_analysis.R
+++ run_analysis.R
@@ -20,6 +20,7 @@ download.file(fileUrl,destfile="./originalData/Dataset.zip")
 unzip(zipfile="./originalData/Dataset.zip",exdir="./originalData")
 
 # packages required to run this script
+# if needed use commands install.packages("data.table") and/or install.packages("dplyr") to get the required packages
 library(data.table)
 library(dplyr)
 
@@ -60,9 +61,33 @@ merged_data <- cbind(subject, activity, features)
 
 # Step 2: Extracting the measurements on the mean and standard deviation for each measurement
 
-# subsetting columns that include mean() and std() at the end
-# (This is my interpretations since there are no specific marking criteria on the number of columns.)
+# Create a vector for defining columns that include mean() or std() at the end
+# (This is my interpretation since there are no specific marking criteria on the number of columns.)
 
+columns_mean_std <- grep("std\\(\\)|mean\\(\\)", names(merged_data), ignore.case = TRUE)
 
+# add subject and activity columns to the list
 
+selected_columns <- c(1, 2, columns_mean_std)
 
+# create a subset that includes only selected columns
+
+subset_data <- merged_data[,selected_columns]
+
+# There are now 10299 rows and 68 columns in the merged and extracted data frame
+# dim(subset_data)
+# [1] 10299    68
+
+# Step 3: Using descriptive activity names to name the activities
+
+# change the activity variable from integer to a factor variable and add labels
+
+subset_data$activity <- factor(subset_data$activity,
+                               levels = c(1:6),
+                               labels = (activity_labels[,2]))
+
+# translate characters in activity column from upper to lower case so that it looks tidier
+
+subset_data$activity <- tolower(subset_data$activity)
+
+# Step 4: Labeling the data set with descriptive variable names.
